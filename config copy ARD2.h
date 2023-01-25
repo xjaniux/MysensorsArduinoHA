73f8bab40c011d78243ecf5cccee8f45f1a96242
xjaniux <116461895+xjaniux@users.noreply.github.com>
2023-01-25 20:51:24 +0100

    Update config copy ARD2.h

--- config copy ARD2.h
+++ config copy ARD2.h
@@ -25,7 +25,7 @@ const unsigned long BUTTON_LONG_PRESS_INTERVAL = 800;
   const uint8_t gExpanderAddresses[] = {0x20};
 #endif
 
-const char MULTI_RELAY_DESCRIPTION[] PROGMEM = "Multi Relay";
+const char MULTI_RELAY_DESCRIPTION[] PROGMEM = "Przekazniki";
 
 // Relays config - ID and PIN MUST BE UNIQUE (can't be repeated)!
 // Row params: relay ID - [0-254] sensor ID reported on MySensor Gateway
@@ -34,12 +34,12 @@ const char MULTI_RELAY_DESCRIPTION[] PROGMEM = "Multi Relay";
 //             relay dependOn - ID of relay that needs to be turned on before this one
 //             relay description - reported on MySensor Gateway, can help identify device on initial configuration in Home Automation App, max. 30 chars, can be empty ("")
 const RelayConfigDef gRelayConfig[] PROGMEM = {
-  {33, 2, RELAY_TRIGGER_LOW, -1, "RELAY 33"},
-  {34, 3, RELAY_TRIGGER_LOW, -1, "RELAY 34"},
-  {35, 4, RELAY_TRIGGER_LOW, -1, "RELAY 35"},
-  {36, 5, RELAY_TRIGGER_LOW, -1, "RELAY 36"},
-  {37, 6, RELAY_TRIGGER_LOW, -1, "RELAY 37"},
-  {38, 7, RELAY_TRIGGER_LOW, -1, "RELAY 38"},
+  {33, 2, RELAY_TRIGGER_HIGH | RELAY_IMPULSE, -1, "Brama Garaż"}, //A0, 43 Brama Garażowa
+  {34, 3, RELAY_TRIGGER_HIGH, -1, "Drzwi Garaż"},
+  {35, 4, RELAY_TRIGGER_HIGH, -1, "Drzwi Spiżarnia"},
+  {36, 5, RELAY_TRIGGER_HIGH, -1, "Drzwi ŁazienkaD"},
+  {37, 6, RELAY_TRIGGER_HIGH, -1, "Drzwi ŁazienkaM"},
+  {38, 7, RELAY_TRIGGER_HIGH, -1, "Drzwi Garderoba"},
   {39, 8, RELAY_TRIGGER_LOW, -1, "RELAY 39"},
   {40, 9, RELAY_TRIGGER_LOW, -1, "RELAY 40"},
   {41, 10, RELAY_TRIGGER_LOW, -1, "RELAY 41"},
@@ -65,12 +65,12 @@ const RelayConfigDef gRelayConfig[] PROGMEM = {
 //             double-click action relay ID - ignored for DING_DONG, REED_SWITCH
 //             button description - debug only, max. 30 chars, can be empty ("")
 const ButtonConfigDef gButtonConfig[] PROGMEM = {
-  {A0,  MONO_STABLE, 33,  -1, -1, "BUTTON 33"},
-  {A1,  MONO_STABLE, 34,  -1, -1, "BUTTON 34"},
-  {A2,  MONO_STABLE, 35,  -1, -1, "BUTTON 35"},
-  {A3,  MONO_STABLE, 36,  -1, -1, "BUTTON 36"},
-  {A4,  MONO_STABLE, 37,  -1, -1, "BUTTON 37"},
-  {A5,  MONO_STABLE, 38,  -1, -1, "BUTTON 38"},
+  {A0,  MONO_STABLE, 33,  -1, -1, "Brama Garaż"},
+  {A1,  REED_SWITCH, 34,  -1, -1, "Drzwi Garaż"},
+  {A2,  REED_SWITCH, 35,  -1, -1, "Drzwi Spiżarnia"},
+  {A3,  REED_SWITCH, 36,  -1, -1, "Drzwi ŁazienkaD"},
+  {A4,  REED_SWITCH, 37,  -1, -1, "Drzwi ŁazienkaM"},
+  {A5,  REED_SWITCH, 38,  -1, -1, "Drzwi Garderoba"},
   {A6,  MONO_STABLE, 39,  -1, -1, "BUTTON 39"},
   {A7,  MONO_STABLE, 40,  -1, -1, "BUTTON 40"},
   {A8,  MONO_STABLE, 41,  -1, -1, "BUTTON 41"},
@@ -81,25 +81,6 @@ const ButtonConfigDef gButtonConfig[] PROGMEM = {
   {A13,  MONO_STABLE, 46,  -1, -1, "BUTTON 46"},
   {A14,  MONO_STABLE, 47,  -1, -1, "BUTTON 47"},
   {A15,  MONO_STABLE, 48,  -1, -1, "BUTTON 48"}
-  //{38,  MONO_STABLE, 48,  -1, -1, "BUTTON 49"},
-  //{39,  MONO_STABLE, 18,  -1, -1, "BUTTON 50"},
-  //{40,  MONO_STABLE, 19,  -1, -1, "BUTTON 51"},
-  //{41,  MONO_STABLE, 20,  -1, -1, "BUTTON 20"},
-  //{42,  MONO_STABLE, 21,  -1, -1, "BUTTON 21"},
-  //{43,  MONO_STABLE, 22,  -1, -1, "BUTTON 22"},
-  //{44,  MONO_STABLE, 23,  -1, -1, "BUTTON 23"},
-  //{45,  MONO_STABLE, 24,  -1, -1, "BUTTON 24"},
-  //{46,  MONO_STABLE, 25,  -1, -1, "BUTTON 25"},
-  //{47,  MONO_STABLE, 26,  -1, -1, "BUTTON 26"},
-  //{48,  MONO_STABLE, 27,  -1, -1, "BUTTON 27"},
-  //{49,  MONO_STABLE, 28,  -1, -1, "BUTTON 28"},
-  //{50,  MONO_STABLE, 29,  -1, -1, "BUTTON 29"},
-  //{51,  MONO_STABLE, 30,  -1, -1, "BUTTON 30"},
-  //{52,  MONO_STABLE, 31,  -1, -1, "BUTTON 31"},
-  //{53,  MONO_STABLE, 32,  -1, -1, "BUTTON 32"}
-    
-  //{A3,  BI_STABLE,   4,  -1, -1, "BUTTON 4"},
-  //{A4,  BI_STABLE,   5,  -1,  2, "BUTTON 5"},
-  //{A5,  REED_SWITCH, 8,  -1, -1, "BUTTON 6"}
+
 };
 
